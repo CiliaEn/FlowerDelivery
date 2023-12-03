4fe4969b56dd9b35b147825f7d0441da24da2e73
ciliaence <cilia.ence@telia.com>
2023-12-03 13:31:20 +0100

    mv firebaseconfig

---
+++ app/firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCtlqAn1RNACiu-JycOyR-SU78Zs-ddXKE",
+  authDomain: "flowerdelivery-104ad.firebaseapp.com",
+  projectId: "flowerdelivery-104ad",
+  storageBucket: "flowerdelivery-104ad.appspot.com",
+  messagingSenderId: "956124951873",
+  appId: "1:956124951873:web:de71be784d54bd772a9dc0",
+  measurementId: "G-7C7RDNB251",
+};
--- app/firebase/firebaseManager.ts
+++ app/firebase/firebaseManager.ts
@@ -8,16 +8,7 @@ import {
 } from "firebase/firestore";
 import { Store } from "../types";
 import mockStores from "../mockData/mockStores";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCtlqAn1RNACiu-JycOyR-SU78Zs-ddXKE",
-  authDomain: "flowerdelivery-104ad.firebaseapp.com",
-  projectId: "flowerdelivery-104ad",
-  storageBucket: "flowerdelivery-104ad.appspot.com",
-  messagingSenderId: "956124951873",
-  appId: "1:956124951873:web:de71be784d54bd772a9dc0",
-  measurementId: "G-7C7RDNB251",
-};
+import { firebaseConfig } from "./firebaseConfig";
 
 const initializeFirebase = () => {
   const app = initializeApp(firebaseConfig);
